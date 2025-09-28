72815571439d34594a6e09b7e4a9c66540149de4
Álvaro <alvaromg8m@gmail.com>
2025-09-28 19:50:26 +0200

    Implemented interior of house

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Teleport : MonoBehaviour
+{
+
+    [SerializeField] private Transform _destination;
+    [SerializeField] private CinemachineVirtualCamera _exteriorCam;
+    [SerializeField] private CinemachineVirtualCamera _interiorCam;
+    [SerializeField] private Animator _fadeAnimator;
+    
+
+    private void OnTriggerEnter2D(Collider2D other){
+
+        if(other.TryGetComponent(out PlayerController player)){
+
+            _fadeAnimator.Play("fadeBlack");
+
+
+            //_exteriorCam.Priority = 0;
+            //_interiorCam.Priority = 10;
+
+            StartCoroutine(fadeBlack());
+            other.transform.position = _destination.position;
+
+            //other.transform.position = _destination.position;
+
+        }
+    }
+
+    private IEnumerator fadeBlack(){
+        //yield return new WaitForSeconds(0.5f);
+        yield return null;
+        _exteriorCam.Priority = 0;
+        _interiorCam.Priority = 10;
+    }
+
+}
